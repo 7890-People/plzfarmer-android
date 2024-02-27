4e32510af5f21fafa460d5ef865749d15510f37c
lee-june-young <uhiccup@gmail.com>
2024-02-27 12:21:25 +0900

    feat: 빈 homeFragment 생성

---
+++ app/src/main/java/com/konkuk/plzfarmer/presentation/main/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.konkuk.plzfarmer.presentation.main.home
+
+import com.konkuk.plzfarmer.R
+import com.konkuk.plzfarmer.databinding.FragmentHomeBinding
+import com.konkuk.plzfarmer.presentation.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override val TAG: String = "HomeFragment"
+    override val layoutRes: Int = R.layout.fragment_home
+
+    override fun afterViewCreated() {
+
+    }
+}
