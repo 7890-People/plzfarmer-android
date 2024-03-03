a5142ba3f5468b747850c055c1edd490b06a6fdf
lee-june-young <uhiccup@gmail.com>
2024-03-03 14:46:21 +0900

    style: 커뮤니티 상세 화면 작업

---
+++ app/src/main/java/com/konkuk/plzfarmer/presentation/main/community/CommunityDetailFragment.kt
@@ -0,0 +1,16 @@
+package com.konkuk.plzfarmer.presentation.main.community
+
+import com.konkuk.plzfarmer.R
+import com.konkuk.plzfarmer.databinding.FragmentCommunityBinding
+import com.konkuk.plzfarmer.databinding.FragmentCommunityDetailBinding
+import com.konkuk.plzfarmer.presentation.base.BaseFragment
+
+class CommunityDetailFragment : BaseFragment<FragmentCommunityDetailBinding>() {
+    override val TAG: String = "CommunityDetailFragment"
+    override val layoutRes: Int = R.layout.fragment_community_detail
+
+    override fun afterViewCreated() {
+
+    }
+
+}
