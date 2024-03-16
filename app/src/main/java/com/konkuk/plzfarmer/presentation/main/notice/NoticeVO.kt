e80ab279e49a36ae0145734dd7c9f68c61862238
lee-june-young <uhiccup@gmail.com>
2024-03-16 20:29:54 +0900

    feat: 알림 조회 api 초기 작업

---
+++ app/src/main/java/com/konkuk/plzfarmer/presentation/main/notice/NoticeVO.kt
@@ -0,0 +1,9 @@
+package com.konkuk.plzfarmer.presentation.main.notice
+
+class NoticeVO (
+    //알림 recyclerView 아이템 데이터
+    val noticeId: Int,
+    val noticeType: String,
+    val noticeContent1: String,
+    val noticeContent2: String
+)
